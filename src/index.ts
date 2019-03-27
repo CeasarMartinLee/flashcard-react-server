5577f4d204cd055b68ab8d45de2495078a7b7276
“Ceasar” <lee_ceasar@yahoo.com>
2019-03-27 13:54:23 +0100

    server compiled and running

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import 'reflect-metadata'
+import {createKoaServer} from 'routing-controllers'
+import setupDb from './db'
+
+
+
+const app = createKoaServer({
+  cors: true,
+  controllers: [
+
+  ],
+  // authorizationChecker: async (action: Action, roles: string[]) => {
+  //   const header: string = action.request.headers.authorization;
+
+  //   if (header && header.startsWith('Bearer ')) {
+  //     const [, token] = header.split(' ');
+  //     if(token) {
+  //       const userId = verify(token).data.id
+  //       const user = await User.findOne(userId)
+  //         //https://github.com/typestack/routing-controllers#authorized-decorator
+  //       if(user && !roles.length) 
+  //         return true
+  //       if (user && roles.find(roles => user.roles.indexOf(roles) !== -1))
+  //         return true;
+
+  //     }
+  //   }
+  //   return false;
+  // },
+  // authorizationChecker: (action: Action) => {
+  //   const header: string = action.request.headers.authorization
+  //   if (header && header.startsWith('Bearer ')) {
+  //     const [ , token ] = header.split(' ')
+  //     try {
+  //       return !!(token && verify(token))
+  //     }
+  //     catch (e) {
+  //       throw new BadRequestError(e)
+  //     }    }
+  //   return false
+  // },
+  // currentUserChecker: async (action: Action) => {
+  //   const header: string = action.request.headers.authorization
+  //   if (header && header.startsWith('Bearer ')) {
+  //     const [ , token ] = header.split(' ')
+  //     if (token) {
+  //       const userId = verify(token).data.id;
+  //       const user = await User.findOne(userId);
+  //       return user
+  //     }
+  //   }
+  //   return undefined
+  // }
+})
+
+setupDb()
+  .then(_ =>
+    app.listen(process.env.PORT|| 4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
